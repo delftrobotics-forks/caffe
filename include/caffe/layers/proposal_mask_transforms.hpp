13e8a0b4eb77fb6a6853a6237e96edc6737fa3d1
Mihai Morariu <m.a.morariu@delftrobotics.com>
2017-02-08 14:19:05 +0100

    Fix segfault in intersectMask

--- include/caffe/layers/proposal_mask_transforms.hpp
+++ include/caffe/layers/proposal_mask_transforms.hpp
@@ -52,38 +52,45 @@ namespace algorithms {
 
   /** \brief Calculates the intersection of an external box and ground truth box
              and masks it according to gt_mask.
-   *  \param [in]  external    Input external box.
-   *  \param [in]  annotated   Input ground truth box.
+   *  \param [in]  ex_box      Input external box.
+   *  \param [in]  gt_box      Input ground truth box.
    *  \param [in]  mask        Input mask.
    *  \param [in]  mask_size   Mask size (after resizing).
    *  \param [in]  threshold   Binarization threshold.
    *  \return Regression targets.
    */
   template <typename T>
-  cv::Mat_<T> intersectMask(cv::Rect_<T> const & external,
-                            cv::Rect_<T> const & annotated,
+  cv::Mat_<T> intersectMask(cv::Rect_<T> const & ex_box,
+                            cv::Rect_<T> const & gt_box,
                             cv::Mat_<T>  const & mask,
                             int          const   mask_size,
                             float        const   threshold)
   {
-    int x1 = std::round(std::max(external.tl().x, annotated.tl().x));
-    int y1 = std::round(std::max(external.tl().y, annotated.tl().y));
-    int x2 = std::round(std::min(external.br().x, annotated.br().x));
-    int y2 = std::round(std::min(external.br().y, annotated.br().y));
+    int x1 = std::round(std::max(ex_box.tl().x, gt_box.tl().x));
+    int y1 = std::round(std::max(ex_box.tl().y, gt_box.tl().y));
+    int x2 = std::round(std::min(ex_box.br().x, gt_box.br().x));
+    int y2 = std::round(std::min(ex_box.br().y, gt_box.br().y));
 
     if (x1 > x2 || y1 > y2) { return cv::Mat_<T>::zeros(mask_size, mask_size); }
 
-    cv::Point external_tl (std::round(x1 -  external.tl().x), std::round(y1 -  external.tl().y));
-    cv::Point annotated_tl(std::round(x1 - annotated.tl().x), std::round(y1 - annotated.tl().y));
-    cv::Size size(x2 - x1 + 1, y2 - y1 + 1);
+    cv::Point ex_box_tl(std::round(x1 - ex_box.tl().x), std::round(y1 - ex_box.tl().y));
+    cv::Point gt_box_tl(std::round(x1 - gt_box.tl().x), std::round(y1 - gt_box.tl().y));
+    cv::Size size      (x2 - x1 + 1, y2 - y1 + 1);
 
-    cv::Mat_<T> intersect_mask = mask(cv::Rect(annotated_tl.x, annotated_tl.y, size.width, size.height));
+    cv::Rect boundaries = {0, 0, mask.cols, mask.rows};
+    cv::Rect roi        = cv::Rect(gt_box_tl.x, gt_box_tl.y, size.width, size.height) & boundaries;
+    cv::Mat_<T> intersect_mask = mask(roi);
 
-    cv::Size regression_size(std::round(external.br().x - external.tl().x + 1),
-                             std::round(external.br().y - external.tl().y + 1));
+    cv::Size regression_size(std::round(ex_box.br().x - ex_box.tl().x + 1),
+                             std::round(ex_box.br().y - ex_box.tl().y + 1));
 
-    cv::Mat_<T> regression = cv::Mat_<T>::zeros(regression_size);
-    intersect_mask.copyTo(regression(cv::Rect(external_tl.x, external_tl.y, size.width, size.height)));
+    cv::Mat_<T> regression  = cv::Mat_<T>::zeros(regression_size);
+    boundaries              = {0, 0, regression.cols, regression.rows};
+    roi                     = cv::Rect(ex_box_tl.x, ex_box_tl.y, size.width, size.height);
+    cv::Rect new_boundaries = roi | boundaries;
+
+    regression.create(new_boundaries.height, new_boundaries.width);
+    intersect_mask.copyTo(regression(roi));
     cv::resize(regression, regression, cv::Size(mask_size, mask_size));
 
     return regression >= threshold;
